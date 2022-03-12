5227ccc490b521b1a918015b2c66eb9e73f9aff5
Nithyadevi <nithi2218@gmail.com>
2022-03-12 22:55:47 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const name = (['Lisa', 'Kaitlin', 'Jan'], 'surprise');
+
+function writeCards(name, surprise) {
+    let newName = [];
+    for(let i=0; i<name.length; i++) {
+        newName.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return newName;
+}
+
+
+
+function countDown(){
+let num=6;    
+    while (num > 0) {
+        console.log(num);
+        num--;
+    }
+let num1=4;    
+    while (num1 >= 0) {
+        console.log(num1);
+        num1--;
+    }
+
+}
